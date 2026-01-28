0766809accb1c5878c500fb0e3806c6cfb3c19fe
claire803 <lewisc8@rpi.edu>
2026-01-28 13:31:45 -0500

    Update arbitrary.java

--- arbitrary.java
+++ arbitrary.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class arbitrary {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This will create a merge conflict.");
     }
 }
